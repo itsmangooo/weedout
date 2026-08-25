822f61fc0714d95991142e1f569c3cfcb218113e
itsmangooo <emanuel.rajic@unvi.edu.ba>
2026-08-25 13:00:49 +0200

    Add Weedout brand logo component

---
+++ frontend/src/components/brand/WeedoutLogo.jsx
@@ -0,0 +1,19 @@
+export function WeedoutLogo() {
+  return (
+    <>
+      <svg
+        aria-hidden="true"
+        className="weedout-brand__mark"
+        focusable="false"
+        viewBox="0 0 256 256"
+      >
+        <g fill="currentColor">
+          <path d="M34 46 L78 46 L126 146 L96 206 Z" />
+          <path d="M92 46 L136 46 L184 146 L154 206 Z" />
+          <path d="M146 46 L194 46 L170 92 Z" />
+        </g>
+      </svg>
+      <span className="weedout-brand__wordmark">WEEDOUT</span>
+    </>
+  );
+}
